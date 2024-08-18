b44ea7222302253fbf41db57771eecfb75dbc76b
Christian <cteguimtu620@gmail.com>
2024-08-18 12:59:53 -0400

    .

--- Random_number_guessing.py
+++ Random_number_guessing.py
@@ -1,14 +1,17 @@
 from random import randrange
+y = 0
 x = input("What would like the range to be in: ")
 n = randrange(int(x))
 if n < 10:
     print("The range has to be above 10.")
 else:
     print("Have fun guessing the Mystery Number!\nType what you think is the correct number below:")
+
     while True:
         v = int(input())
         if n == v:
-            print('You guessed the Mystery Number!')
+            print('You guessed the Mystery Number in', y + 1, "tries")
             break
         print('Smaller' if (n < v) else 'Bigger')
+        y += 1
 
